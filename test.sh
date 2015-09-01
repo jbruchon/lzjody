2596e5e3f4f71328271411b8bb65f9c44f53f448
Jody Bruchon <jody@c02ware.com>
2015-09-01 15:35:46 -0400

    test.sh: improve testing and expand number of test cases

--- test.sh
+++ test.sh
@@ -15,6 +15,9 @@ test -x lzjody.static && LZJODY=./lzjody.static
 
 test ! -x $LZJODY && echo "Compile the program first." && clean_exit 1
 
+# For running e.g. Valgrind
+test -z "$1" || LZJODY="$@ $LZJODY"
+
 CFAIL=0; DFAIL=0
 $LZJODY -c < $IN > $COMP 2>log.test.compress || CFAIL=1
 if [ $CFAIL -eq 0 ]
@@ -34,14 +37,20 @@ test "$S1" != "$S2" && echo -e "\nCompressor/decompressor tests FAILED: mismatch
 
 # Out-of-bounds length tests
 
+: > log.test.invalid
+echo -n "Testing invalid (large) lengths...";
+echo "Large length test:" >> log.test.invalid
 echo -en '\x10\x05\xaf\xff' > $TF
 dd if=/dev/zero bs=4095 count=1 2>/dev/null >> $TF
-./lzjody.static -d < $TF 2>/dev/null && \
-	echo -e "\nDecompressor invalid (large) length test FAILED.\n" && clean_exit 1
+$LZJODY -d < $TF 2>> log.test.invalid && echo "FAILED" && clean_exit 1
+echo "passed"
 
-echo -en '\x00\x00\x00' | ./lzjody.static -d 2>/dev/null && \
-	echo -e "\nDecompressor invalid (zero) length test FAILED.\n" && clean_exit 1
-
+echo -n "Testing invalid (zero) lengths...";
+echo "Zero length test:" >> log.test.invalid
+echo -en '\x00\x00\x00' | \
+$LZJODY -d 2>> log.test.invalid && echo "FAILED" && clean_exit 1
+echo "passed"
 
 ### All tests passed!
 echo -e "\nCompressor/decompressor tests PASSED.\n"
+clean_exit
